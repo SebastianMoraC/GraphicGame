e825380f833f4825f060b160b76a5325a14c61de
SebastianMoraC <sebastian.mora@utp.edu.co>
2020-11-11 15:35:26 -0500

    third commit

--- game.py
+++ game.py
@@ -1,1 +1,19 @@
-py
+import pygame, sys, json
+
+from pygame.draw import circle
+
+WIDTH = 600
+HEIGHT =600
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH,HEIGHT))
+    pygame.display.set_caption("Game")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+if __name__ == "__main__":
+    main()
