089b013d3c8fc49b30a05dea78b6dfba939b3a36
SebastianMoraC <sebastian.mora@utp.edu.co>
2020-11-11 22:28:04 -0500

    casi está listo

--- game.py
+++ game.py
@@ -1,19 +1,109 @@
 import pygame, sys, json
-
+import player
 from pygame.draw import circle
 
-WIDTH = 600
-HEIGHT =600
 
+
+TILEWIDTH = 0
+TILEHEIGHT = 0
+MAPWIDTH = 0
+MAPHEIGHT = 0
+MATRIZMAP = []
+WIDTH = 800
+HEIGHT = 800
+speed = 2
+trueScroll = [0,0]
+
+def uploadMap(map):
+    #Open File
+    global TILEWIDTH, TILEHEIGHT, MAPWIDTH, MAPHEIGHT, MATRIZMAP
+    file = open(map+".json","r")
+    data = json.load(file)
+    file.close
+
+    #Get Size
+    TILEWIDTH = data["tilewidth"]
+    TILEHEIGHT = data["tileheight"]
+    MAPWIDTH = data["width"]
+    MAPHEIGHT = data["height"]
+
+    #Get Map
+    map = []
+    for item in data["layers"]:
+        map = item["data"] #Get the map
+
+
+    for i in range(0,len(map),MAPWIDTH):
+        MATRIZMAP.append(map[i:i+MAPWIDTH])
+'''    for i in range(MAPHEIGHT):
+        print(MATRIZMAP[i])'''
+#cut
+def cut(image, rectangle):
+    rect = pygame.Rect(rectangle)
+    imgCut = pygame.Surface(rect.size).convert()
+    imgCut.blit(image,(0,0),rect)
+    return imgCut
+
+def arrayTileSet(image):
+    x,y = 0,0
+    leftFiles =[]
+
+    for i in range(30):
+        for j in range(27):
+            imageCut = cut(image,(x,y,16,16))
+            leftFiles.append(imageCut)
+            x += 18
+        x = 0
+        y += 18
+        
+    return leftFiles
 
 def main():
     pygame.init()
+    
+    #Screen
     screen = pygame.display.set_mode((WIDTH,HEIGHT))
     pygame.display.set_caption("Game")
+    
+    #Player
+    myPlayer = player.Xena((50,345),speed)
+    
+    #Map
+    img = pygame.image.load("images/worldMario.png")
+    uploadMap("maps/mapGG")
+    left = arrayTileSet(img)
+
+    #Font and Text
+    white = pygame.Color("#FFFFFF")
+    fontOne = pygame.font.Font(None,37)
+    textOne = fontOne.render("Time:",0,(200,0,150))
+    timeAux = 1
     while True:
+        
+        screen.blit(myPlayer.clipping,myPlayer.rectClip)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        myPlayer.eventS(event)
+        
+        timeS = pygame.time.get_ticks() //1000      #The time is miliseconds so i divide in 1000
+        if(timeAux == timeS):
+            print(timeAux)
+            timeAux += 1
+
+        for i in range(MAPHEIGHT):
+            for j in range(MAPWIDTH):
+                numTile = MATRIZMAP[i][j]
+                tileImg = left[numTile-1]
+                tileImg = pygame.transform.scale(tileImg,(TILEWIDTH, TILEHEIGHT))
+                screen.blit(tileImg, (j*TILEWIDTH, i*TILEHEIGHT))
+        screen.blit(myPlayer.clipping,(myPlayer.rectClip.x,myPlayer.rectClip.y))
+
+        #Count
+        count = fontOne.render("Time left: " + str(150-timeS),0,white)    #Write the cont: 
+        screen.blit(count,(625,10)) #Show the cont in the screen
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,78 @@
+import pygame, sys
+from pygame.draw import rect
+from pygame.locals import *
+
+
+class Xena(pygame.sprite.Sprite):
+    def __init__(self, position,speed):
+        self.speed = speed
+        self.player = pygame.image.load('images/xena.png')
+        self.player.set_clip(pygame.Rect(0,0,52,76)) #Look in the paint the direction to rect the image in the sprite - This is to select the clip
+        self.clipping = self.player.subsurface(self.player.get_clip())
+        self.rectClip = self.clipping.get_rect()
+        self.rectClip.topleft = position
+        self.figure = 0
+        self.statesLeft = {0: (0,76,52,76), 1: (52,76,52,76), 2: (156,76,52,76)}
+        self.statesRight = {0: (0,152,52,76), 1: (52,152,52,76), 2: (156,152,52,76)} #Coords (x,y,width,heigth)
+        self.statesUp = {0: (0,228,52,76), 1: (52,228,52,76), 2: (156,228,52,76)}
+        self.statesDown = {0: (0,0,52,76), 1: (52,0,52,76), 2: (156,0,52,76) }
+
+    def get_figure(self, states):
+        self.figure += 1
+        if self.figure >= (len(states)):
+            self.figure = 0
+        return states[self.figure]
+    
+    def cut(self, rectClipping):
+        if type(rectClipping) is dict:
+            self.player.set_clip(pygame.Rect(self.get_figure(rectClipping)))
+        else:
+            self.player.set_clip(pygame.Rect(rectClipping))
+        return rectClipping 
+
+    def updateSprite(self, direction):
+        if direction == "left":
+            self.cut(self.statesLeft)
+            self.rectClip.x -= self.speed 
+        elif direction == "right":
+            self.cut(self.statesRight)
+            self.rectClip.x += self.speed 
+        elif direction == "up":
+            self.cut(self.statesUp)
+            self.rectClip.y -= self.speed 
+        elif direction == "down":
+            self.cut(self.statesDown)
+            self.rectClip.y += self.speed 
+        
+        elif direction == "stopLeft":
+            self.cut(self.statesLeft[0])
+        elif direction == "stopRight":
+            self.cut(self.statesRight[0])
+        elif direction == "stopUp":
+            self.cut(self.statesUp[0])
+        elif direction == "stopDown":
+            self.cut(self.statesDown[0])
+        
+        self.clipping = self.player.subsurface(self.player.get_clip())
+
+    def eventS(self, event):
+        if event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                self.updateSprite("left")
+            elif event.key == K_RIGHT:
+                self.updateSprite("right")
+            elif event.key == K_DOWN:
+                self.updateSprite("down")
+            elif event.key == K_UP:
+                self.updateSprite("up")
+
+        elif event.type == KEYUP: #Dont Push
+            if event.key == K_LEFT:
+                self.updateSprite("stopLeft")
+            elif event.key == K_RIGHT:
+                self.updateSprite("stopRight")
+            elif event.key == K_DOWN:
+                self.updateSprite("stopDown")
+            elif event.key == K_UP:
+                self.updateSprite("stopUp")
+    
